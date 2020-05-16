c93c6762b4effb1cc9731f98fbd5b8fe46a34794
Dom <46989387+dvitelli@users.noreply.github.com>
2020-05-15 19:53:18 -0700

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+struct node {
+
+	int payload; 
+	struct  node *next;
+
+	};
+
+class linkedList {
+private:
+
+	node *head, *tail;
+
+public:
+
+	linkedList(){
+
+		head = NULL;
+		tail = NULL;
+
+		}
+
+	void addNode(int input) {
+
+		node *temp = new node; //set up temp
+		temp->payload = input;
+		temp->next = NULL;
+
+		if (head == NULL) { //check if head null
+
+			head = temp;
+			tail = temp;
+			
+		} else { //if not add to end
+
+			tail->next = temp;
+			tail = tail->next;
+		
+		}
+
+	} void printList() {
+
+		node* temp = new node;
+		temp = head;
+		while (temp != NULL) {
+
+			std::cout << temp->payload << std::endl;
+			temp = temp->next;
+		}
+		
+	
+	}
+};
+
+int main() {
+
+	linkedList test;
+
+	for (int i = 0; i < 10000; i++) {
+
+		int rng = rand() % 100;
+		test.addNode(rng);
+
+	}
+
+	test.printList();
+
+	return 0;
+}

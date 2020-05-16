bb33ba3eacff714470b149bb2ae845a5d34f72dd
Dom <46989387+dvitelli@users.noreply.github.com>
2020-05-15 19:52:53 -0700

    Create Queue.cpp

---
+++ Queue.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+struct node {
+
+	int payload;
+	struct  node* next;
+
+};
+
+class queue {
+private:
+
+	node *front, *rear;
+
+public:
+
+	queue() {
+
+		front = NULL;
+		rear = NULL;
+
+	}
+
+	void enqueue(int input) {
+
+		node* temp = new node; //set up temp
+		temp->payload = input;
+		temp->next = NULL;
+
+		if (front == NULL) { //check if head null
+
+			front = temp;
+			rear = temp;
+			
+
+		}
+		else { //if not add to end
+
+			rear->next = temp;
+			rear = rear->next;
+			
+		}
+
+	} void dequeue() {
+		
+		if (front == NULL) {
+
+			return;
+		}
+
+		front = front->next;
+		
+
+		if (front == NULL) {
+
+			front = NULL;
+			rear = NULL;
+		}
+
+	} void printList() {
+
+		node* temp = new node;
+		temp = front;
+		int counter = 1;
+
+		while (temp != NULL) {
+
+			std::cout << "Payload: "<< temp->payload << " ";
+			std::cout << " Postion: "  << counter++ << " in queue. " << std::endl;
+			temp = temp->next;
+		}
+
+
+	}
+};
+
+int main() {
+
+	//queue test;
+
+	//test.enqueue(4);
+	//test.enqueue(5);
+	//test.enqueue(6);
+	//test.dequeue();
+	//test.enqueue(7);
+	//test.dequeue();
+	
+	
+
+	//test.printList();
+
+	return 0;
+}
